e229a643cfebf0d4309735bc511184419845cd68
kovalevandrey007 <kovalevandrey007@gmail.com>
2022-11-27 20:02:33 +0200

    Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -5,5 +5,9 @@
 782 -> 8
 918 -> 1*/
 
-if(99>a $$ a<1000)
-b=a%100
+Console.WriteLine ("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a>99 && a<1000)
+{a= a%100/10;
+Console.WriteLine(a);}
